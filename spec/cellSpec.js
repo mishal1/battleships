a9e9cb5c35fa8ae9d126267408e467bee8a163d1
Mishal Islam <mishal.islam01@gmail.com>
2015-05-08 15:12:29 +0100

    added cell object

---
+++ spec/cellSpec.js
@@ -0,0 +1,40 @@
+describe ('Cell', function(){
+
+  var cell;
+
+  beforeEach(function(){
+    cell = new Cell();
+  });
+
+  it('has no ship when initialised', function(){
+    expect(cell.hasShip).toBe(false);
+  });
+
+  describe('place ship', function(){
+
+    beforeEach(function(){
+      cell.placeShip();
+    })
+
+    it('can have a ship', function(){
+      expect(cell.hasShip).toBe(true);
+    });
+
+    it('can remove ship', function(){
+      cell.removeShip();
+      expect(cell.hasShip).toBe(false);
+    });
+    
+  });
+
+  it('is not hit when initialised', function(){
+    expect(cell.isHit).toBe(false)
+  });
+
+  it('can be hit', function(){
+    cell.hit()
+    expect(cell.isHit).toBe(true)
+  });
+
+
+});
---
+++ src/cell.js
@@ -0,0 +1,16 @@
+function Cell(){
+  this.hasShip = false;
+  this.isHit = false;
+}
+
+Cell.prototype.placeShip = function() {
+  this.hasShip = true;
+};
+
+Cell.prototype.removeShip = function() {
+  this.hasShip = false;
+};
+
+Cell.prototype.hit = function() {
+  this.isHit = true;
+};
