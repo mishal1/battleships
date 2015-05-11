3b1d83c09183ab1b7c98b63fb8edfa629c0cd6b1
Mishal Islam <mishal.islam01@gmail.com>
2015-05-11 23:11:25 +0100

    declares winner

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,10 +5,6 @@ describe('Game', function(){
   var game, board, player, ships;
 
   beforeEach(function(){
-    // board = {
-    //   allShipsSunk: function(){
-    //   return true;
-    // }};
     ship = jasmine.createSpyObj('ship', ['']);
     game = new Game([ship]);
   });
@@ -24,47 +20,75 @@ describe('Game', function(){
   describe('when a player is added', function(){
 
     beforeEach(function(){
-      player = {
-        ready: function(){
-          return true;
-        }
-      };
-      game.add(player);
+      player1 = jasmine.createSpyObj('player', ['ready', 'hit']);
+      player1.ready = function(){ return true; };
+      player1.lost = function(){ return true; };
+      player2 = jasmine.createSpyObj('player', ['ready', 'hit']);
+      player2.ready = function(){ return true; };
+      player2.lost = function(){ return false; };
+      game.add(player1);
     });
 
     it('has a player', function(){
-      expect(game.players).toEqual([player]);
+      expect(game.players).toEqual([player1]);
     });
 
     it('is not ready when there is one player', function(){
       expect(game.ready()).toBe(false);
     });
 
-    it('is ready when both players are ready', function(){
-      game.add(player);
-      expect(game.ready()).toBe(true);
+    describe('add another player', function(){
+
+      beforeEach(function(){
+        game.add(player2);
+      });
+
+      it('is ready when both players are ready', function(){
+        expect(game.ready()).toBe(true);
+      });
+      
+      it('the current player is the position 0', function(){
+        expect(game.currentPos).toEqual(0);
+      });
+
+      it('has a current player', function(){
+        expect(game.currentPlayer()).toEqual(player1);
+      });
+
+      it('the current player is the position 0', function(){
+        expect(game.opponentPos).toEqual(1);
+      });
+
+      it('has an opponent', function(){
+        expect(game.opponent()).toEqual(player2);
+      });
+
+      describe('has a turn', function(){
+
+        beforeEach(function(){
+          game.turn(1);
+        });
+
+        it('player hits opponents board', function(){
+          expect(player1.hit).toHaveBeenCalled();
+        });
+  
+        it('the current player is the position 1', function(){
+          expect(game.currentPos).toEqual(1);
+        });
+
+        it('the opponent player is the position 0', function(){
+          expect(game.opponentPos).toEqual(0);
+        });
+
+        it('declares a winner', function(){
+          expect(game.turn(1)).toEqual(player2);
+        });
+
+      });
+
     });
 
   });
-
-  // describe('check if it is over', function(){
-
-  //   it('check if ships have sunk', function(){
-  //     spyOn(board, "allShipsSunk");
-  //     game.checkIfWon();
-  //     expect(board.allShipsSunk).toHaveBeenCalled();
-  //   });
-
-  //   it('all ships have sunk', function(){
-  //     expect(game.checkIfWon()).toEqual('Game over');
-  //   });
-
-  //   it('all ships have not sunk', function(){
-  //     spyOn(board, "allShipsSunk");
-  //     expect(game.checkIfWon()).toEqual(false);
-  //   });
-    
-  // });
-
 
 });
