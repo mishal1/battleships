51cf712c72836e0242728ff0438cd4237cebf1ad
Mishal Islam <mishal.islam01@gmail.com>
2015-05-20 17:13:06 +0100

    restarted

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,18 +1,1 @@
 var socket = io();
-
-socket.on('start game', function(msg){
-  $('#opponentboard').css("display", "inline-block");
-  $('.boardtext').css("display", "block");
-  $('#placeshipstext').text(' ');
-  $.ajax({
-    url: '/turn',
-    type: 'POST',
-    data: {},
-    success: function(data){
-    }
-  });
-});
-
-var turn = function(){
-  
-}
